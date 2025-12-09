1148cda3e19df7e2225316600e4712da4b434865
Abhishek Parashar <aparashar103@gmail.com>
2025-12-10 00:13:51 +0530

    project setup

---
+++ spring-concepts/src/main/java/com/example/prac/reflection/BasicReflection.java
@@ -0,0 +1,10 @@
+package com.example.prac.reflection;
+
+import java.lang.reflect.Method;
+
+public class BasicReflection {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello reflection");
+    }
+}
+
